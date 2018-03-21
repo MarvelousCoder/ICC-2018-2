de61444c163bbb09ed5d7c4e5ae2e2a0b9c20688
Angelic Coder <Raphael_soares_1996@hotmail.com>
2018-03-21 19:49:15 -0300

    update

--- Provas/Prova 1/Provas Tipo B/Prova 1.4/Prova 1.4 - Tipo B.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-custo = 0
-x = int(input())
-maior = x
-while(n > 1): #Os valores das próximas n-1 casas
-    x = int(input())
-    custo = custo + (maior - x)
-    n -= 1
-print(custo)
