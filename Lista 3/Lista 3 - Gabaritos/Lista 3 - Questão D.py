ef58f0afb0a74a00070a656e8cb04d3aa242ada0
Angelic Coder <Raphael_soares_1996@hotmail.com>
2018-04-01 18:01:00 -0300

    Lista 2 e 3 atualizadas.

--- Lista 3/Lista 3 - Gabaritos/Lista 3 - Questão D.py
+++ Lista 3/Lista 3 - Gabaritos/Lista 3 - Questão D.py
@@ -1,34 +1,40 @@
-n = int(input())
-x, y = input().split()
-x, y = [int(x), int(y)]
-soma = 0
-while(y > 0):
-        if(x % 2 != 0):
-            soma += x
-            y -= 1
-        x += 1
-maior = soma
-menor = soma
-print(soma)
 """Pegamos a soma da 1º dupla de X e Y como maior e menor soma, depois vamos
 comparando com as somas das outras duplas. """
-while(n > 1): #Lemos os próximos n-1 casos de teste
+def main():
+    n = int(input())
     x, y = input().split()
     x, y = [int(x), int(y)]
     soma = 0
-    while(y > 0):#While *
+    while(y > 0):
         if(x % 2 != 0):
             soma += x
             y -= 1
         x += 1
-    if(soma > maior):
-        maior = soma
-    if(soma < menor):
-        menor = soma
+    maior = soma 
+    menor = soma
     print(soma)
-    n -= 1
-print(maior)
-print(menor)
+    while(n > 1): #Lemos os próximos n-1 casos de teste
+        x, y = input().split()
+        x, y = [int(x), int(y)]
+        soma = 0
+        while(y > 0):#While *
+                if(x % 2 != 0):
+                        soma += x
+                        y -= 1
+                x += 1
+        if(soma > maior):
+                maior = soma
+        if(soma < menor):
+                menor = soma
+        print(soma)
+        n -= 1
+    print(maior)
+    print(menor)
+    print("%.2f"%((maior+menor)/2))
+    
+if __name__ == '__main__':
+    main()
+
 """ Para o while *, se o atual x for ímpar, então somamos ao valor de soma e
 atribuímos a variável soma. Também decrementamos o valor de y, pois dos Y
 ímpares consecutivos já atribuímos 1 a soma. Depois incrementamos x e continua
